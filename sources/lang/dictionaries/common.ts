87a923ef2e7c4c631693b3eb453059a59c271213
Павел Ткаченко <paveltkachenko.my@gmail.com>
2022-07-21 15:37:15 +0300

    rename folder

--- sources/lang/dictionaries/common.ts
+++
@@ -1,6 +0,0 @@
-import { EditableTextByToken } from "../editable-text-by-token";
-
-export type CommonDictionary = {
-	testString: string;
-	testEditableString: EditableTextByToken<{token: string}>;
-};
--- sources/libs/lang.ts
+++
@@ -1,8 +0,0 @@
-import { ExpressApi } from "./exepress-api";
-import { LangAndDictionary, Langs } from "../lang";
-
-export let Lang: LangAndDictionary;
-export const LangMiddleware = (request: ExpressApi.IRequest, response: ExpressApi.IResponse, next: ExpressApi.INext) => {
-	Lang = new LangAndDictionary(request.cookies.lang || Langs.en);
-	next();
-};
